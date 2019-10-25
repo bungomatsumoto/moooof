1952af3d88bd89f96ce5f002c5b1cee8ea06f8d1
bungo matsumoto <matsumotobg@gmail.com>
2019-10-25 14:19:59 +0900

    index画面を作成

--- app/controllers/properties_controller.rb
+++ app/controllers/properties_controller.rb
@@ -1,18 +1,20 @@
 class PropertiesController < ApplicationController
   def index
+    @properties = Property.all
   end
 
   def new
     @property = Property.new
-    @station = Station.new
+    @station = @property.stations.build
   end
 
   def create
-
+    Property.create(property_params)
+    redirect_to new_property_path
   end
 
   def show
-
+    @property = Property.find(params[:id])
   end
 
   def edit
@@ -21,5 +23,11 @@ class PropertiesController < ApplicationController
 
   def destroy
 
+  end
+
+  private
+
+  def property_params
+    params.require(:property).permit(:name, :rent, :address, :age, :notes, stations_attributes:[:route, :station_name, :distance])
   end
 end
--- app/models/property.rb
+++ app/models/property.rb
@@ -1,5 +1,5 @@
 class Property < ApplicationRecord
   has_many :stations, through: :property_stations
   has_many :property_stations, dependent: :destroy
-  accepts_nested_attributes_for :property_stations, allow_destroy: true
+  accepts_nested_attributes_for :stations, allow_destroy: true
 end
