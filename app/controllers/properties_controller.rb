ed7cfa8c789b4fd501b7c92912b50644ca24ee00
bungo matsumoto <matsumotobg@gmail.com>
2019-10-25 18:22:51 +0900

    編集画面を作成

--- app/controllers/properties_controller.rb
+++ app/controllers/properties_controller.rb
@@ -1,28 +1,42 @@
 class PropertiesController < ApplicationController
+  before_action :get_it_property, only: [:show, :edit, :update, :destroy]
+
   def index
     @properties = Property.all
   end
 
   def new
     @property = Property.new
-    @station = @property.stations.build
+    @property.stations.build
   end
 
   def create
-    Property.create(property_params)
-    redirect_to new_property_path
+    @property = Property.new(property_params)
+    if @property.save
+      redirect_to properties_path
+    else
+      render :new
+    end
   end
 
   def show
-    @property = Property.find(params[:id])
+    binding.pry
   end
 
   def edit
+  end
 
+  def update
+    if @property.update(property_params)
+      redirect_to property_path
+    else
+      render :edit
+    end
   end
 
   def destroy
-
+    @property.destroy
+    redirect_to properties_path, notice:"物件情報を削除しました！"
   end
 
   private
@@ -30,4 +44,9 @@ class PropertiesController < ApplicationController
   def property_params
     params.require(:property).permit(:name, :rent, :address, :age, :notes, stations_attributes:[:route, :station_name, :distance])
   end
+
+  def get_it_property
+    @property = Property.find(params[:id])
+  end
+
 end
